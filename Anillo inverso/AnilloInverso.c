0dcaa35792af74d0752202d4ceba5fa52871eb25
LuisEnriqueSosaHernandez <kiquesasuke@gmail.com>
2019-04-02 21:47:19 -0600

    Anillos

--- Anillo inverso/AnilloInverso.c
+++ Anillo inverso/AnilloInverso.c
@@ -17,45 +17,45 @@ int main(int argc,char * argv[]){
 		}else{
 		switch(rango){
 			case 0:
-			printf("Anillo Estatico\n");
-			destino=1;
-			fuente=4;
-			sprintf(cad,"Yo soy P%d recibiendo mensaje de",destino);
-			MPI_Send(cad,strlen(cad)+1,MPI_CHAR,destino,etiqueta,MPI_COMM_WORLD);
+			destino=4;
+			fuente=1;
 			MPI_Recv(mensaje,max,MPI_CHAR,fuente,etiqueta,MPI_COMM_WORLD,&estado);
 			printf("%s P%d\n",mensaje,estado.MPI_SOURCE);
+			sprintf(cad,"Yo soy P%d recibiendo mensaje de",destino);
+			MPI_Send(cad,strlen(cad)+1,MPI_CHAR,destino,etiqueta,MPI_COMM_WORLD);
 			break;
 			case 1:
-			destino=2;
-			fuente=0;
-			MPI_Recv(mensaje,100,MPI_CHAR,fuente,etiqueta,MPI_COMM_WORLD,&estado);
-			printf("%s P%d\n",mensaje,estado.MPI_SOURCE);
-			sprintf(cad,"Yo soy P%d recibiendo mensaje de",destino);
-			MPI_Send(cad,strlen(cad)+1,MPI_CHAR,destino,etiqueta,MPI_COMM_WORLD);
-			break;
-			case 2:
-			destino=3;
-			fuente=1;
-			MPI_Recv(mensaje,100,MPI_CHAR,fuente,etiqueta,MPI_COMM_WORLD,&estado);
-			printf("%s P%d\n",mensaje,estado.MPI_SOURCE);
-			sprintf(cad,"Yo soy P%d recibiendo mensaje de",destino);
-			MPI_Send(cad,strlen(cad)+1,MPI_CHAR,destino,etiqueta,MPI_COMM_WORLD);
-			break;
-			case 3:
-			destino=4;
+			destino=0;
 			fuente=2;
 			MPI_Recv(mensaje,100,MPI_CHAR,fuente,etiqueta,MPI_COMM_WORLD,&estado);
 			printf("%s P%d\n",mensaje,estado.MPI_SOURCE);
 			sprintf(cad,"Yo soy P%d recibiendo mensaje de",destino);
 			MPI_Send(cad,strlen(cad)+1,MPI_CHAR,destino,etiqueta,MPI_COMM_WORLD);
 			break;
-			case 4:
-			destino=0;
+			case 2:
+			destino=1;
 			fuente=3;
 			MPI_Recv(mensaje,100,MPI_CHAR,fuente,etiqueta,MPI_COMM_WORLD,&estado);
 			printf("%s P%d\n",mensaje,estado.MPI_SOURCE);
 			sprintf(cad,"Yo soy P%d recibiendo mensaje de",destino);
 			MPI_Send(cad,strlen(cad)+1,MPI_CHAR,destino,etiqueta,MPI_COMM_WORLD);
+			break;
+			case 3:
+			destino=2;
+			fuente=4;
+			MPI_Recv(mensaje,100,MPI_CHAR,fuente,etiqueta,MPI_COMM_WORLD,&estado);
+			printf("%s P%d\n",mensaje,estado.MPI_SOURCE);
+			sprintf(cad,"Yo soy P%d recibiendo mensaje de",destino);
+			MPI_Send(cad,strlen(cad)+1,MPI_CHAR,destino,etiqueta,MPI_COMM_WORLD);
+			break;
+			case 4:
+			printf("Anillo Estatico\n");
+			destino=3;
+			fuente=0;
+			sprintf(cad,"Yo soy P%d recibiendo mensaje de",destino);
+			MPI_Send(cad,strlen(cad)+1,MPI_CHAR,destino,etiqueta,MPI_COMM_WORLD);
+			MPI_Recv(mensaje,100,MPI_CHAR,fuente,etiqueta,MPI_COMM_WORLD,&estado);
+			printf("%s P%d\n",mensaje,estado.MPI_SOURCE);
 			break;
 		}
 	}
